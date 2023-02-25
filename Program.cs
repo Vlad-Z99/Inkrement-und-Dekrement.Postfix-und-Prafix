3ba30ec8fa3c3006ed430bf4da55c33f6b485c52
Vladislav Zanfirov <126167687+Vlad-Z99@users.noreply.github.com>
2023-02-25 19:41:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * Inkrement und Dekrement. Postfix und Präfix
+ */
+
+namespace myLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 1;
+
+            a = ++a * a;
+            b = b++ * b;                    
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
